6cf8f8f7c0b1e6c06e68ab4fad94eefeb53de2a9
Emanuel Martinez <emanuelmartinezw@hotmail.com>
2022-04-03 21:24:43 -0300

    feat: env added, vars naming changed

--- helpers.py
+++ helpers.py
@@ -1,29 +1,34 @@
+import json
+import os
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from datetime import datetime
-import json
+from dotenv import load_dotenv
 
-def scrapSingleJobPage(url):
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+load_dotenv()
+
+def scrap_job_page_by_url(url):
+    chromedriver = os.getenv('SELENIUM_DRIVER_PATH')
+    driver = webdriver.Chrome(chromedriver)
     driver.get(url)
-    header = driver.find_element(By.XPATH, '//div[@class="header__content header__content--full-width content-block"]')
 
+    header = driver.find_element(By.XPATH, '//div[@class="header__content header__content--full-width content-block"]')
     title = header.find_element(By.XPATH, './/h1[@class="content-block__title"]').text
-    publishedAt = header.find_elements(By.XPATH, './/li[@class="contact-details__link"]')[1].text
-    finishAt = header.find_element(By.XPATH, './/li[@class="contact-details__link text--alternative"]').text
+    published_at = header.find_elements(By.XPATH, './/li[@class="contact-details__link"]')[1].text
+    finish_at = header.find_element(By.XPATH, './/li[@class="contact-details__link text--alternative"]').text
     description = driver.find_element(By.XPATH, '//div[@class="body-copy hidden--from-l"]').find_element(By.XPATH, './/div[@class="content"]').get_attribute('innerHTML')
 
     driver.quit()
 
     return {
+        "url": url,
         "title": title,
-        "publishedAt": publishedAt,
-        "finishAt": finishAt,
+        "published_at": published_at,
+        "finish_at": finish_at,
         "description": description,
     }
 
-def exportJobsToJson(data):
+def export_to_json(data):
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
     fileName = 'exports/' + timestamp + '_jobs.json'
     
--- main.py
+++ main.py
@@ -1,23 +1,43 @@
-from fileinput import filename
+import time
+import os
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from helpers import scrapSingleJobPage, exportJobsToJson
+from helpers import scrap_job_page_by_url, export_to_json
+from selenium.common.exceptions import NoSuchElementException   
+from dotenv import load_dotenv
 
-url = 'https://www.randstad.com.ar/trabajos/buenos-aires/bahia-blanca/'
-driver = webdriver.Chrome(ChromeDriverManager().install())
+load_dotenv()
 
-data = []
+jobs_scraped = []
+url = os.getenv('RANDSTAD_URL')
+chromedriver = os.getenv('SELENIUM_DRIVER_PATH')
+
+driver = webdriver.Chrome(chromedriver)
 driver.get(url)
-jobs = driver.find_element(By.XPATH, '//ul[@class="cards__list cards__list--format-grid"]').find_elements(By.XPATH, './/li[@class="cards__item"]');
 
-for job in jobs:
+time.sleep(5)
+cookies_alert = driver.find_element(By.XPATH, '//button[@id="onetrust-accept-btn-handler"]')
+if cookies_alert:
+    cookies_alert.click()
+
+has_next_page = True
+while has_next_page:
+    try:
+        next_page_button = driver.find_element(By.XPATH, '//button[@class="button button--m bluex-button--preloader"]')
+        driver.execute_script("arguments[0].click();", next_page_button)
+    except NoSuchElementException:
+        has_next_page = False
+
+jobs_list = driver.find_element(By.XPATH, '//ul[@class="cards__list cards__list--format-grid"]').find_elements(By.XPATH, './/li[@class="cards__item"]');
+
+for job in jobs_list:
     header = job.find_element(By.XPATH, './/div[@class="cards__header"]').find_element(By.XPATH, './/h3[@class="cards__title"]')
-    jobUrl = header.find_element(By.XPATH, './/a[@class="cards__link"]').get_attribute('href');
+    job_url = header.find_element(By.XPATH, './/a[@class="cards__link"]').get_attribute('href');
 
-    data.append(scrapSingleJobPage(jobUrl))
+    job_details = scrap_job_page_by_url(job_url)
+    jobs_scraped.append(job_details)
 
 driver.quit()
 
-exportJobsToJson(data)
-print('Randstad jobs scrapped :D')
+export_to_json(jobs_scraped)
+print('Randstad jobs scraped :D')
